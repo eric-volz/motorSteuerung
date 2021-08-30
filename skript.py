f68d4ad1f11cb4670c6fe8e498392fc85cae772d
eric-volz <eric.Volz02@gmail.com>
2021-08-30 15:47:59 +0200

    first commit

---
+++ skript.py
@@ -0,0 +1,15 @@
+import time
+import Rpi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+A = 7
+B = 11
+C = 13
+D = 15
+
+GPIO.setup(A, GPIO.OUT)
+GPIO.setup(B, GPIO.OUT)
+GPIO.setup(C, GPIO.OUT)
+GPIO.setup(D, GPIO.OUT)
