d4d15e1f9ec2cc92cf10b830a7b5c2325b936d3a
eric-volz <eric.Volz02@gmail.com>
2021-08-30 16:09:56 +0200

    fertiges skript

--- skript.py
+++ skript.py
@@ -13,3 +13,48 @@ GPIO.setup(A, GPIO.OUT)
 GPIO.setup(B, GPIO.OUT)
 GPIO.setup(C, GPIO.OUT)
 GPIO.setup(D, GPIO.OUT)
+
+def GPIO_SETUP(a, b, c, d):
+    GPIO.output(A, a)
+    GPIO.output(B, b)
+    GPIO.output(C, c)
+    GPIO.output(D, d)
+    time.sleep(0.001)
+
+def RIGTH_TURN(deg):
+    full_circle = 510.0
+    degree = full_circle/360*deg
+    GPIO_SETUP(0, 0, 0, 0)
+
+    while degree > 0.0:
+        GPIO_SETUP(1, 0, 0, 0)
+        GPIO_SETUP(1, 1, 0, 0)
+        GPIO_SETUP(0, 1, 0, 0)
+        GPIO_SETUP(0, 1, 1, 0)
+
+        GPIO_SETUP(0, 0, 1, 0)
+        GPIO_SETUP(0, 0, 1, 1)
+        GPIO_SETUP(0, 0, 0, 1)
+        GPIO_SETUP(1, 0, 0, 1)
+        degree -= -1
+
+def LEFT_TURN(deg):
+    full_circle = 510.0
+    degree = full_circle/360*deg
+    GPIO_SETUP(0, 0, 0, 0)
+
+    while degree > 0.0:
+        GPIO_SETUP(1, 0, 0, 1)
+        GPIO_SETUP(0, 0, 0, 1)
+        GPIO_SETUP(0, 0, 1, 1)
+        GPIO_SETUP(0, 0, 1, 0)
+        GPIO_SETUP(0, 1, 1, 0)
+        GPIO_SETUP(0, 1, 0, 0)
+        GPIO_SETUP(1, 1, 0, 0)
+        GPIO_SETUP(1, 0, 0, 0)
+        degree -= -1
+
+RIGTH_TURN(90)
+LEFT_TURN(90)
+RIGTH_TURN(90)
+GPIO_SETUP(0, 0, 0, 0)
